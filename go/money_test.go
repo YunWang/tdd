3ee6751fcf5922ca12cb4d44ab584b7f519f2d01
yunwang <yunwang@ebay.com>
2022-04-30 18:55:01 +0800

    feat: division and multiplication features done

--- go/money_test.go
+++ go/money_test.go
@@ -2,10 +2,29 @@ package main
 
 import "testing"
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{amount: 5}
-	tenner := fiver.Times(2)
-	if tenner.amount != 10 {
-		t.Errorf("Expected 10, got: [%d]", tenner.amount)
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{amount: 5, currency: "USD"}
+	actualResult := fiver.Times(2)
+	expectedResult := Money{amount: 10, currency: "USD"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualMoneyAfterDivision := originalMoney.Divide(4)
+	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
