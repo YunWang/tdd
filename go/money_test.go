73b03080c966aceabda7f07df3141daabef8356f
yunwang <yunwang@ebay.com>
2022-05-03 19:07:04 +0800

    feat: conversion of Money from EUR to USD

--- go/money_test.go
+++ go/money_test.go
@@ -3,46 +3,61 @@ package main
 import (
 	"testing"
 
-	"github.com/yunwang/tdd/go/stocks"
+	s "github.com/yunwang/tdd/go/stocks"
 )
 
 func TestMultiplicationInDollars(t *testing.T) {
-	fiver := stocks.NewMoney(5, "USD")
+	fiver := s.NewMoney(5, "USD")
 	actualResult := fiver.Times(2)
-	expectedResult := stocks.NewMoney(10, "USD")
+	expectedResult := s.NewMoney(10, "USD")
 	assertEqual(t, expectedResult, actualResult)
 }
 
 func TestMultiplicationInEuros(t *testing.T) {
-	tenEuros := stocks.NewMoney(10, "EUR")
+	tenEuros := s.NewMoney(10, "EUR")
 	actualResult := tenEuros.Times(2)
-	expectedResult := stocks.NewMoney(20, "EUR")
+	expectedResult := s.NewMoney(20, "EUR")
 	assertEqual(t, expectedResult, actualResult)
 }
 
 func TestDivision(t *testing.T) {
-	originalMoney := stocks.NewMoney(4002, "KRW")
+	originalMoney := s.NewMoney(4002, "KRW")
 	actualMoneyAfterDivision := originalMoney.Divide(4)
-	expectedMoneyAfterDivision := stocks.NewMoney(1000.5, "KRW")
+	expectedMoneyAfterDivision := s.NewMoney(1000.5, "KRW")
 	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
 }
 
-func assertEqual(t *testing.T, expected stocks.Money, actual stocks.Money) {
+func assertEqual(t *testing.T, expected s.Money, actual s.Money) {
 	if expected != actual {
 		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
 
 func TestAddition(t *testing.T) {
-	var portfolio stocks.Portfolio
-	var portfolioInDollars stocks.Money
+	var portfolio s.Portfolio
+	var portfolioInDollars s.Money
 
-	fiveDollars := stocks.NewMoney(5, "USD")
-	tenDollars := stocks.NewMoney(10, "USD")
-	fifteenDollars := stocks.NewMoney(15, "USD")
+	fiveDollars := s.NewMoney(5, "USD")
+	tenDollars := s.NewMoney(10, "USD")
+	fifteenDollars := s.NewMoney(15, "USD")
 	portfolio = portfolio.Add(fiveDollars)
 	portfolio = portfolio.Add(tenDollars)
 	portfolioInDollars = portfolio.Evaluate("USD")
 
 	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
+func TestAdditionOfDollarsAndEuros(t *testing.T) {
+	var portfolio s.Portfolio
+
+	fiveDollars := s.NewMoney(5, "USD")
+	tenEuros := s.NewMoney(10, "EUR")
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenEuros)
+
+	expectedValue := s.NewMoney(17, "USD")
+	actualValue := portfolio.Evaluate("USD")
+
+	assertEqual(t, expectedValue, actualValue)
 }
